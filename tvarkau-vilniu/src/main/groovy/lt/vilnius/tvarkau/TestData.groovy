cfb2ea2b888ab4589b7c9fb32f4710aa7c0008ba
Tadas Krivickas <tadas.krivickas@gmail.com>
2015-11-22 19:13:53 +0200

    Add test data for dev profile

---
+++ tvarkau-vilniu/src/main/groovy/lt/vilnius/tvarkau/TestData.groovy
@@ -0,0 +1,35 @@
+package lt.vilnius.tvarkau
+
+import groovy.util.logging.Slf4j
+import lt.vilnius.tvarkau.user.User
+import lt.vilnius.tvarkau.user.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+import javax.annotation.PostConstruct
+
+import static lt.vilnius.tvarkau.common.Profiles.dev
+
+@Slf4j
+@Profile(dev)
+@Configuration
+class TestData {
+
+    @Autowired
+    UserRepository userRepository
+
+    @PostConstruct
+    void seed() {
+        log.info 'dev profile detected, populating test data'
+        testUser().with(userRepository.&save)
+    }
+
+    private User testUser() {
+        new User(
+                token: UUID.fromString('00000000-0000-0000-0000-000000000000'),
+                enabled: true
+        )
+    }
+
+}
