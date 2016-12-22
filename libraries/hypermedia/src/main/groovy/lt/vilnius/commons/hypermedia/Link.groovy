bc61acc8f0ad5f43372688453968b29f427440fd
Tadas Krivickas <tadas.krivickas@gmail.com>
2016-12-23 00:58:17 +0200

    Provide short form for creating self links

--- libraries/hypermedia/src/main/groovy/lt/vilnius/commons/hypermedia/Link.groovy
+++ libraries/hypermedia/src/main/groovy/lt/vilnius/commons/hypermedia/Link.groovy
@@ -5,5 +5,11 @@ import groovy.transform.builder.SimpleStrategy
 
 @Builder(builderStrategy = SimpleStrategy, prefix = '')
 class Link {
+    static final String REL_SELF = 'self'
+
     String rel, href
+
+    static Link selfLink(String href) {
+        return new Link(rel: REL_SELF, href: href)
+    }
 }
