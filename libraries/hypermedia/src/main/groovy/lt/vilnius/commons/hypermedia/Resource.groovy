e96afe67d4e6f279d1edbfca39f21876e452596a
Tadas Krivickas <tadas.krivickas@gmail.com>
2016-12-23 00:54:45 +0200

    Serialize resource links HAL-style

--- libraries/hypermedia/src/main/groovy/lt/vilnius/commons/hypermedia/Resource.groovy
+++ libraries/hypermedia/src/main/groovy/lt/vilnius/commons/hypermedia/Resource.groovy
@@ -1,6 +1,9 @@
 package lt.vilnius.commons.hypermedia
 
+import com.google.gson.annotations.SerializedName
+
 class Resource {
+    @SerializedName('_links')
     final Map<String, Link> links = [:]
 
     Resource linkTo(Link link) {
--- libraries/hypermedia/src/test/groovy/lt/vilnius/commons/hypermedia/ResourceSpec.groovy
+++ libraries/hypermedia/src/test/groovy/lt/vilnius/commons/hypermedia/ResourceSpec.groovy
@@ -1,5 +1,7 @@
 package lt.vilnius.commons.hypermedia
 
+import com.google.gson.Gson
+import groovy.json.JsonSlurper
 import spock.lang.Specification
 
 class ResourceSpec extends Specification {
@@ -8,9 +10,22 @@ class ResourceSpec extends Specification {
         given:
             def resource = new TestResource()
         when:
-            resource.linkTo(new Link().rel('something').href('/something'))
+            resource.linkTo(new Link().rel('something').href('/somewhere'))
         then:
             resource.links.something
-            resource.links.something.href == '/something'
+            resource.links.something.href == '/somewhere'
+    }
+
+    def 'serializes per HAL spec'() {
+        given:
+            def resource = new TestResource()
+        when:
+            resource.linkTo(new Link().rel('something').href('/somewhere'))
+            String resourceJson = new Gson().toJson(resource)
+            Map parsedJson = new JsonSlurper().parseText(resourceJson) as Map
+        then:
+            parsedJson._links
+            parsedJson._links.something.rel == 'something'
+            parsedJson._links.something.href == '/somewhere'
     }
 }
