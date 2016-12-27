a17c22442fb9bfd89d5d1549e62c7652225dca75
Tadas Krivickas <tadas.krivickas@gmail.com>
2016-12-28 00:39:26 +0200

    Add LocalDate support to Gson

--- app/src/main/groovy/lt/vilnius/tvarkau/Config.groovy
+++ app/src/main/groovy/lt/vilnius/tvarkau/Config.groovy
@@ -1,15 +1,19 @@
 package lt.vilnius.tvarkau
 
 import com.google.gson.Gson
-import spark.Filter
-import spark.Request
-import spark.Response
-import spark.ResponseTransformer
-import spark.Route
+import com.google.gson.GsonBuilder
+import lt.vilnius.tvarkau.util.LocalDateGsonAdapter
+import spark.*
+
+import java.time.LocalDate
 
 class Config {
 
-    static final Gson GSON = new Gson()
+    static final Gson GSON = new GsonBuilder()
+        .registerTypeAdapter(LocalDate, new LocalDateGsonAdapter())
+        .setPrettyPrinting()
+        .create()
+
     static final ResponseTransformer JSON = GSON.&toJson as ResponseTransformer
 
     static Filter defaultContentTypeFilter() {
---
+++ libraries/util/src/test/groovy/lt/vilnius/tvarkau/util/LocalDateGsonAdapterSpec.groovy
@@ -0,0 +1,42 @@
+package lt.vilnius.tvarkau.util
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import groovy.json.JsonSlurper
+import spock.lang.Shared
+import spock.lang.Specification
+
+import java.time.LocalDate
+
+import static java.time.Month.MAY
+
+class LocalDateGsonAdapterSpec extends Specification {
+
+    @Shared
+    Gson gson = new GsonBuilder()
+        .registerTypeAdapter(LocalDate, new LocalDateGsonAdapter())
+        .create()
+
+    def 'serializes/deserializes local date'() {
+        when:
+            String json = gson.toJson(new DateHolder(
+                localDate: LocalDate.of(1999, MAY, 20)
+            ))
+            Map parsedJson = new JsonSlurper().parseText(json) as Map
+        then:
+            parsedJson.localDate == '1999-05-20'
+
+        when:
+            DateHolder parsedDate = gson.fromJson(json, DateHolder)
+        then:
+            with(parsedDate.localDate) {
+                year == 1999
+                month == MAY
+                dayOfMonth == 20
+            }
+    }
+
+    static class DateHolder {
+        LocalDate localDate
+    }
+}
