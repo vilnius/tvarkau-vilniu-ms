1e1a014389789e1465aa7a76e2d9f4ce660a5b47
Tadas Krivickas <tadas.krivickas@gmail.com>
2016-12-28 02:02:50 +0200

    Configure logging

---
+++ app/src/main/resources/logback.groovy
@@ -0,0 +1,7 @@
+appender('CONSOLE', ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = '%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n'
+    }
+}
+
+root(INFO, ['CONSOLE'])
