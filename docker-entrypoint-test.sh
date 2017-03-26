3337bb392b49b3a1737aabb983578f3161caede4
Tadas Krivickas <tadas.krivickas@gmail.com>
2017-03-26 23:12:40 +0300

    Re-setup Travis CI

--- docker-entrypoint-test.sh
+++ docker-entrypoint-test.sh
@@ -2,7 +2,14 @@
 
 echo "Running test entrypoint..."
 
-rake db:drop 2>/dev/null
-rake db:setup
+rake db:migrate 2>/dev/null || rake db:setup && rake db:test:prepare
 
 bundle exec rspec spec
+
+if [ -n "$PULL_REQUEST_ID" ]; then
+    echo "Got PULL_REQUEST_ID=${PULL_REQUEST_ID}, running pronto with github_pr reporter..."
+    pronto run -f github_pr
+else
+    echo "Got no PULL_REQUEST_ID, running pronto with default reporter..."
+    pronto run
+fi
