3da19e5fd7b7d4fdb256107e617b73f409e838d1
Tadas Krivickas <tadas.krivickas@gmail.com>
2017-03-27 01:26:15 +0300

    Detect non-PR builds

--- docker-entrypoint-test.sh
+++ docker-entrypoint-test.sh
@@ -6,7 +6,7 @@ rake db:migrate 2>/dev/null || rake db:setup && rake db:test:prepare
 
 bundle exec rspec spec
 
-if [ -n "$PULL_REQUEST_ID" ] && [ -n "$PRONTO_GITHUB_ACCESS_TOKEN" ]; then
+if [ -n "$PULL_REQUEST_ID" ] && [ "$PULL_REQUEST_ID" != "false" ] && [ -n "$PRONTO_GITHUB_ACCESS_TOKEN" ]; then
     echo "Got an upstream PR #${PULL_REQUEST_ID}. Running pronto with github_pr reporter..."
     pronto run -f github_pr
 else
