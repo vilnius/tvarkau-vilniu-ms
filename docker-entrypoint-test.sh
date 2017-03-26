04649ec7b188f2d681430d2a368fc706ced51818
Tadas Krivickas <tadas.krivickas@gmail.com>
2017-03-26 23:12:40 +0300

    Re-setup Travis CI

--- docker-entrypoint-test.sh
+++ docker-entrypoint-test.sh
@@ -2,7 +2,14 @@
 
 echo "Running test entrypoint..."
 
-rake db:drop 2>/dev/null
-rake db:setup
+rake db:migrate 2>/dev/null || rake db:setup && rake db:test:prepare
 
 bundle exec rspec spec
+
+if [ -n "$PULL_REQUEST_ID" ] && [ -n "$PRONTO_GITHUB_ACCESS_TOKEN" ]; then
+    echo "Got an upstream PR #${PULL_REQUEST_ID}. Running pronto with github_pr reporter..."
+    pronto run -f github_pr
+else
+    echo "Got no PULL_REQUEST_ID or an insecure (fork) PR. Running pronto with default reporter..."
+    pronto run
+fi
