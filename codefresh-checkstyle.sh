870ebe40dd4bd34f0d87cd01e8b3ec03d7fe76f3
Tadas Krivickas <tadas.krivickas@gmail.com>
2017-03-26 14:07:57 +0300

    Log PR number running pronto

--- codefresh-checkstyle.sh
+++ codefresh-checkstyle.sh
@@ -5,11 +5,12 @@ git fetch origin
 
 export PULL_REQUEST_ID=`git branch -r --contains $CF_REVISION 2>/dev/null | sed -n 's/^.*pull\/\([0-9]\+\)$/\1/p'`
 
-echo "PR ID $PULL_REQUEST_ID"
 echo "PR REV $CF_REVISION"
 
 if [ -z "$PULL_REQUEST_ID" ]; then
-    pronto run -f github_status github_pr
+    echo "Got PULL_REQUEST_ID=${PULL_REQUEST_ID}, running pronto with github_pr reporter..."
+    pronto run -f github_pr -c origin/master
 else
+    echo "Got no PULL_REQUEST_ID, running pronto with default reporter..."
     pronto run -c origin/master
 fi
