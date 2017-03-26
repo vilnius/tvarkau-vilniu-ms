769f1da6c4cb9a07fe3d23c5d151fe69d559f82c
Tadas Krivickas <tadas.krivickas@gmail.com>
2017-03-27 00:56:28 +0300

    test

--- docker-entrypoint-test.sh
+++ docker-entrypoint-test.sh
@@ -6,6 +6,8 @@ rake db:migrate 2>/dev/null || rake db:setup && rake db:test:prepare
 
 bundle exec rspec spec
 
+printf "%d\n" ${#PRONTO_GITHUB_ACCESS_TOKEN}
+
 if [ -n "$PULL_REQUEST_ID" ]; then
     echo "Got PULL_REQUEST_ID=${PULL_REQUEST_ID}, running pronto with github_pr reporter..."
     pronto run -f github_pr
