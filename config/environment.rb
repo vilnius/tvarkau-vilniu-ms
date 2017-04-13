69c3fc614818d4772e38ec87674279443b2c2be6
Martynas Jurkus <martynas@vinted.com>
2017-04-13 22:20:50 +0300

    Remove some wawrnings

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 # Load the app's custom environment variables here, so that they are loaded before environments/*.rb
-app_environment_variables = File.join(Rails.root, 'config', 'app_environment_variables.rb')
+app_environment_variables = Rails.root.join('config', 'app_environment_variables.rb')
 load(app_environment_variables) if File.exist?(app_environment_variables)
 
 # Initialize the Rails application.
