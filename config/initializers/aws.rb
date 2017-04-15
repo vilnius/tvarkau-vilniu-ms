d52e23b5c9094d3299312e1dd01235646cc716e9
Martynas Jurkus <martynas@vinted.com>
2017-04-15 10:56:42 +0300

    Use ENV variable for minio endpoint

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,6 @@
 Aws.config.update(
   region: 'us-east-1',
-  endpoint: 'http://localhost:9000',
+  endpoint: ENV['MINIO_ENDPOINT'],
   force_path_style: true,
   credentials: Aws::Credentials.new(
     ENV['MINIO_ACCESS_KEY'],
