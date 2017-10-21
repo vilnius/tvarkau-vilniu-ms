bb08fc79106f6041d92a83988026fef4a2c82add
Martynas Jurkus <martynas@vinted.com>
2017-10-21 20:01:52 +0300

    Update pr

--- config/initializers/tvarkau_vilniu_database.rb
+++ config/initializers/tvarkau_vilniu_database.rb
@@ -1,1 +1,1 @@
-TVARKAU_VILNIU_DB = YAML.load(ERB.new(File.read(Rails.root.join('config', 'database_vilnius.yml'))).result)[Rails.env]
+TVARKAU_VILNIU_DB = YAML.safe_load(ERB.new(File.read(Rails.root.join('config', 'database_vilnius.yml'))).result)[Rails.env]
