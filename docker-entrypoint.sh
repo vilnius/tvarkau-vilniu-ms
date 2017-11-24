89680207b67e03fcc9771f6360e672c5bbaf27a7
Laurynas Butkus <laurynas.butkus@gmail.com>
2017-11-24 22:49:00 +0200

    Remove app pidfile if exists when running in docker container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,6 @@ echo "Running main entrypoint..."
 
 rake db:abort_if_pending_migrations 2>/dev/null || rake db:migrate 2>/dev/null || rake db:setup && rake db:seed
 
+[ -e /opt/tvarkau-vilniu/tmp/pids/server.pid ] && rm /opt/tvarkau-vilniu/tmp/pids/server.pid
+
 exec rails server --port 3000 --binding 0.0.0.0
