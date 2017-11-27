4f6560ee759f374c66005ac73447e321886ccd52
Laurynas Butkus <laurynas.butkus@gmail.com>
2017-11-27 22:15:21 +0200

    Add current_user controller helper (#40)

--- spec/controllers/api/base_controller_spec.rb
+++ spec/controllers/api/base_controller_spec.rb
@@ -8,7 +8,15 @@ RSpec.describe Api::BaseController do
   subject { api_get :index }
 
   describe 'authorization' do
-    it { is_expected.to be_success }
+    context 'with valid token' do
+      let(:oauth_token) { create(:oauth_token, user: user) }
+      let(:user) { create(:user) }
+
+      it 'authorizes' do
+        expect(subject).to be_success
+        expect(controller.send(:current_user)).to eq(user)
+      end
+    end
 
     context 'with invalid token' do
       let(:token) { 'invalid' }
