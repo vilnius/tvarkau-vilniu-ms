5e3e499be5e5a622629aaa0d3e876344f5541d35
Laurynas Butkus <laurynas.butkus@gmail.com>
2017-12-17 11:12:31 +0200

    Refactor report creation interactor (#55)

--- app/domain/reports/create.rb
+++ app/domain/reports/create.rb
@@ -1,25 +1,10 @@
 class Reports::Create
+  include Interactor::Initializer
 
-  def self.for(report, params)
-    new(report, params).run
-  end
-
-  attr_reader :report, :params
-
-  def initialize(report, params)
-    @report, @params = report, params
-  end
+  initialize_with :user, :params
 
   def run
-    report.description = description
-    report.address = address
-    report.lat = latitude
-    report.lng = longitude
-    report.report_type = ReportType.find_by(id: report_type_id)
-
-    report.valid?
-
-    return report if report.errors.any?
+    return report unless report.valid?
 
     report.save
     report
@@ -27,23 +12,7 @@ class Reports::Create
 
   private
 
-  def description
-    params[:description]
-  end
-
-  def address
-    params[:address]
-  end
-
-  def latitude
-    params[:lat]
-  end
-
-  def longitude
-    params[:lng]
-  end
-
-  def report_type_id
-    params[:report_type_id]
+  def report
+    @report ||= user.reports.new(params)
   end
 end
--- spec/domain/reports/create_spec.rb
+++ spec/domain/reports/create_spec.rb
@@ -1,9 +1,41 @@
-RSpec.describe Reports::Create do
-  describe '.for' do
-    subject { described_class.for(user.reports.new, params) }
+# frozen_string_literal: true
 
+RSpec.describe Reports::Create, '.run' do
+  subject { described_class.run(user, params) }
+
+  let(:user) { create(:user) }
+  let(:status) { create(:status) }
+  let(:report_type) { create(:report_type) }
+
+  let(:params) do
+    {
+      report_type_id: report_type.id,
+      status_id: status.id,
+      description: 'Test report',
+      address: 'Test address',
+      lat: 19,
+      lng: 20,
+      # license_plate_no: 'ABC123',
+      registered_at: Time.zone.parse('2017-12-01'),
+    }
+  end
+
+  it 'creates report' do
+    expect(subject).to be_persisted
+    expect(subject).to be_persisted
+    expect(subject.user).to eq(user)
+    expect(subject.report_type).to eq(report_type)
+    expect(subject.status).to eq(status)
+    expect(subject.description).to eq(params[:description])
+    expect(subject.address).to eq(params[:address])
+    expect(subject.lat).to eq(params[:lat])
+    expect(subject.lng).to eq(params[:lng])
+    # expect(subject.license_plate_no).to eq(params[:license_plate_no])
+    expect(subject.registered_at).to eq(params[:registered_at])
+  end
+
+  context 'when report is invalid' do
     let(:params) { {} }
-    let(:user) { create(:user) }
 
     it 'returns report with validation errors' do
       expect(subject.errors).to be_present
