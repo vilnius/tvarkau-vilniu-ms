f2ab3b4c39faf1ad8747502d7592c0000c53d549
Laurynas Butkus <laurynas.butkus@gmail.com>
2017-12-20 00:27:57 +0200

    Add VIISP auth controller

--- config/initializers/viisp_auth.rb
+++ config/initializers/viisp_auth.rb
@@ -6,7 +6,6 @@ VIISP::Auth.configure do |c|
   # if Rails.env.production?
   #   c.pid = ENV['VIISP_PID']
   #   c.private_key = OpenSSL::PKey::RSA.new(Secret['VIISP_PRIVATE_KEY'])
-  #   c.postback_url =
   # else
   #   c.test = true
   # end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  namespace :auth do
+    resource :viisp, controller: :viisp, only: [:new, :create]
+  end
+
   namespace :api, constraints: { format: 'json' } do
     resource :me, controller: :users, only: :show
     resources :reports
