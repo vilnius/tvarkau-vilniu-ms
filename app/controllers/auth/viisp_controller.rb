738a1167c1c7bbd04d57532663b862a4b757772f
Laurynas Butkus <laurynas.butkus@gmail.com>
2017-12-23 23:32:58 +0200

    Add viisp auth redirect uris whitelist (#64)

--- app/controllers/auth/viisp_controller.rb
+++ app/controllers/auth/viisp_controller.rb
@@ -1,10 +1,14 @@
 class Auth::ViispController < ActionController::Base
   include ActionView::Rendering
 
+  ALLOWED_REDIRECT_URIS = ENV['VIISP_REDIRECT_URIS'].to_s.split(',').freeze
+
+  before_action :check_redirect_uri
+
   def new
     postback_url = url_for(
       action: :create,
-      redirect_uri: params[:redirect_uri],
+      redirect_uri: redirect_uri,
     )
 
     @ticket = VIISP::Auth.ticket(postback_url: postback_url)
@@ -13,7 +17,7 @@ class Auth::ViispController < ActionController::Base
   end
 
   def create
-    if params[:redirect_uri].present?
+    if redirect_uri.present?
       redirect_to(redirect_uri_with_ticket)
     else
       render plain: { ticket: params[:ticket] }.to_json, content_type: 'application/json'
@@ -23,12 +27,27 @@ class Auth::ViispController < ActionController::Base
   private
 
   def redirect_uri_with_ticket
-    uri = URI.parse(params[:redirect_uri])
+    uri = URI.parse(redirect_uri)
 
     query = URI.decode_www_form(uri.query || '')
     query << ['ticket', params[:ticket]]
 
     uri.query = URI.encode_www_form(query)
     uri.to_s
+  end
+
+  def check_redirect_uri
+    return if redirect_uri.empty?
+    return if redirect_uri.in?(ALLOWED_REDIRECT_URIS)
+
+    error = "VIISP redirect uri #{redirect_uri} is not allowed"
+
+    Rails.logger.error(error)
+
+    render plain: error, status: :forbidden
+  end
+
+  def redirect_uri
+    params[:redirect_uri]
   end
 end
