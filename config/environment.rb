e0e614efcad3926613cebdd24378b7c5b717e645
Laurynas Butkus <laurynas.butkus@gmail.com>
2017-12-28 21:17:42 +0200

    Load storage config from environment/secrets

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
 
-# Load the app's custom environment variables here, so that they are loaded before environments/*.rb
-template = ERB.new File.new(Rails.root.join('config', 'config.yml')).read
-APP_CONFIG = YAML.load(template.result(binding))[Rails.env]
-
 # Initialize the Rails application.
 Rails.application.initialize!
