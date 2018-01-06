58e0f229a9e182ade23350dab23d3ce5e5dfa034
Laurynas Butkus <laurynas.butkus@gmail.com>
2018-01-07 01:00:32 +0200

    Remove subpath patches

--- config/initializers/0.patches/devise.rb
+++
@@ -1,8 +0,0 @@
-Devise::FailureApp.class_eval do
-  protected
-
-  # RAILS_RELATIVE_URL_ROOT default scope is already set in routes.rb
-  def relative_url_root
-    '/'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,21 @@
 Rails.application.routes.draw do
-  scope Rails.application.config.relative_url_root do
-    use_doorkeeper
+  use_doorkeeper
 
-    root 'welcome#index'
+  root 'welcome#index'
 
-    scope '/auth' do
-      devise_for :users, controllers: { omniauth_callbacks: 'auth/omniauth_callbacks' }
-    end
+  scope '/auth' do
+    devise_for :users, controllers: { omniauth_callbacks: 'auth/omniauth_callbacks' }
+  end
 
-    namespace :auth do
-      resource :viisp, controller: :viisp, only: [:new, :create]
-    end
+  namespace :auth do
+    resource :viisp, controller: :viisp, only: [:new, :create]
+  end
 
-    defaults format: :json do
-      resource :me, controller: :users, only: :show
-      resources :reports
-      resources :report_types, only: [:index]
-      resources :tokens, only: [:create]
-      resources :photos, only: [:create]
-    end
+  defaults format: :json do
+    resource :me, controller: :users, only: :show
+    resources :reports
+    resources :report_types, only: [:index]
+    resources :tokens, only: [:create]
+    resources :photos, only: [:create]
   end
 end
